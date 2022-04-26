62cd984537ef28935b059110827a7aecd394e004
Mbalenhle Ndaba <mbalindaba01@gmail.com>
2022-04-26 08:13:11 +0200

    updated verification function and added the token to local storage

--- index.js
+++ index.js
@@ -1,14 +1,16 @@
 // add code in here to create an API with ExpressJS
 const express = require('express')
+const { verify } = require('jsonwebtoken')
+const jwt = require('jsonwebtoken')
 const garments = require('./garments.json')
 const app = express()
-
+require("dotenv").config();
 app.use(express.static('public'))
 app.use(express.json())
 app.use(express.urlencoded({ extended: false }))
 
 
-app.get('/api/garments', (req, res) => {
+app.get('/api/garments', verifyToken, (req, res) => {
     const gender = req.query.gender
     const season = req.query.season
 
@@ -30,39 +32,46 @@ app.get('/api/garments', (req, res) => {
 })
 
 
-app.post('/api/garments', (req, res) => {
-
-	const {
-		description,
-		img,
-		gender,
-		season,
-		price
-	} = req.body;
-
-	if (!description || !img || !price) {
-		res.json({
-			status: 'error',
-			message: 'Required data not supplied',
-		});
-	} else {
-		garments.push({
-			description,
-			img,
-			gender,
-			season,
-			price
-		});
-
-		res.json({
-			status: 'success',
-			message: 'New garment added.',
-		});
-	}
+app.post('/api/garments', verifyToken, (req, res) => {
+  jwt.verify(req.token, 'secretkey', (err, authData) => {
+    if(err){
+      res.sendStatus(403)
+    }else {
+      const {
+        description,
+        img,
+        gender,
+        season,
+        price
+      } = req.body;
+    
+      if (!description || !img || !price) {
+        res.json({
+          status: 'error',
+          message: 'Required data not supplied',
+          authData
+        });
+      } else {
+        garments.push({
+          description,
+          img,
+          gender,
+          season,
+          price
+        });
+    
+        res.json({
+          status: 'success',
+          message: 'New garment added.',
+          authData
+        });
+      }
+    }
+  })
 });
 
 
-app.get('/api/garments/price/:price', (req, res) => {
+app.get('/api/garments/price/:price', verifyToken, (req, res) => {
     const maxPrice = Number(req.params.price)
 	const filteredGarments = garments.filter(garment => {
 		if (maxPrice > 0) {
@@ -75,6 +84,36 @@ app.get('/api/garments/price/:price', (req, res) => {
 		garments : filteredGarments
 	})
 })
+
+const generateToken = (user) => {
+  return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+    expiresIn: '24h',
+  });
+}
+
+app.post('/auth', (req, res) => {
+  const username = req.query.username
+    if(username == 'mbalindaba01'){
+      console.log('works!')
+      const user = {username: 'mbalindaba01'}
+      const accessToken = generateToken(user)
+      res.json({accessToken: accessToken})
+     }else{
+       throw new Error('Access Denied')
+     }
+})
+
+function verifyToken(req, res, next) {
+  //get auth header value
+  const bearerHeader = req.headers['authorization']
+  if(typeof bearerHeader !== 'undefined'){
+    const token = bearerHeader.split(' ')[1]
+    req.token = token
+    next()
+  }else {
+    res.sendStatus(403)
+  }
+}
 
 const PORT = process.env.PORT || 4017
 
--- public/app.js
+++ public/app.js
@@ -2,11 +2,19 @@
 let seasonFilter = 'All';
 let genderFilter = 'All';
 
+
 const seasonOptions = document.querySelector('.seasons');
 const genderOptions = document.querySelector('.genders');
 const searchResultsElem = document.querySelector('.searchResults');
 const priceRangeElem = document.querySelector('.priceRange');
 const showPriceRangeElem = document.querySelector('.showPriceRange');
+const filterElem = document.querySelector('.filter');
+const addGarmentElem = document.querySelector('.addGarment');
+const garmentsElem = document.querySelector('.garments');
+const loginElem = document.querySelector('.login')
+const usernameRec = document.querySelector('#username')
+const loginBtn = document.querySelector('.loginBtn')
+const errorMessage = document.querySelector('.error-message')
 
 const garmentsTemplateText = document.querySelector('.garmentListTemplate');
 const garmentsTemplate = Handlebars.compile(garmentsTemplateText.innerHTML);
@@ -23,8 +31,13 @@ genderOptions.addEventListener('click', function(evt){
 
 function filterData() {
 	axios
-		.get(`/api/garments?gender=${genderFilter}&season=${seasonFilter}`)
+		.get(`/api/garments?gender=${genderFilter}&season=${seasonFilter}`, {
+			headers: {
+				Authorization: 'Bearer ' + localStorage.getItem('accessToken')
+			}
+		})
 		.then(function(result) {
+			console.log(result)
 			searchResultsElem.innerHTML = garmentsTemplate({
 				garments : result.data.garments
 			})
@@ -37,13 +50,36 @@ priceRangeElem.addEventListener('change', function(evt){
 	const maxPrice = evt.target.value;
 	showPriceRangeElem.innerHTML = maxPrice;
 	axios
-		.get(`/api/garments/price/${maxPrice}`)
+		.get(`/api/garments/price/${maxPrice}`, {
+			headers: {
+				Authorization: 'Bearer ' + localStorage.getItem('accessToken')
+			}
+			})
 		.then(function(result) {
+			console.log(result.data)
 			searchResultsElem.innerHTML = garmentsTemplate({
 				garments : result.data.garments
 			})
 		});
 });
 
-
-filterData();
+function login() {
+	axios
+		.post(`/auth?username=${username.value}`)
+		.then(result => {
+			localStorage.setItem('accessToken', result.data.accessToken)
+			filterElem.classList.toggle('hidden');
+			loginElem.classList.toggle('hidden')
+			addGarmentElem.classList.toggle('hidden')
+			garmentsElem.classList.remove('hide')
+			garmentsElem.classList.toggle('hidden')
+			console.log(garmentsElem)
+		})
+		.catch(err => {
+			console.log(err)
+			errorMessage.style.display = 'block'
+			usernameRec.value = ''
+		})
+}
+loginBtn.addEventListener('click', login)
+filterData()
